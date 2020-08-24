27c72bd6d14b99a286e148e40be58b100c50fb0c
gbox3d <gbox3d@gmail.com>
2020-08-24 17:06:52 +0900

    ...

--- day21/ex07/ex07.cpp
+++ day21/ex07/ex07.cpp
@@ -184,7 +184,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
 		// TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...		
-		
+		//strlen
+		//wcstok
+		//ws -> float -> ws
 		TextOut(hdc, 300, 50, szBuf, wcslen(szBuf));
 
 		EndPaint(hWnd, &ps);
