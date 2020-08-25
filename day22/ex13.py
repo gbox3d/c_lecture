34142495610ea8725ac9d86f0f04538de476dfe6
gbox3d <gbox3d@gmail.com>
2020-08-25 15:50:29 +0900

    ...

---
+++ day22/ex13.py
@@ -0,0 +1,19 @@
+#%%
+from struct import *
+
+print(f'load struct module ')
+
+# %%
+buf = pack('hhl',1,2,3)
+# %%
+_data = unpack('hhl',buf)
+print(_data)
+# %%
+
+_f = open('t1.bin','wb')
+_f.write(buf)
+_f.close()
+
+
+
+# %%
--- day22/readme.txt
+++ day22/readme.txt
@@ -10,3 +10,15 @@ import 모듈이름 as 별명
 sys
 2. sys.path 에 등록된 디랙토리에서 찾는다.
 
+
+파이썬 이진데이터 처리
+인터넷 검색어 : python struct
+
+대문자는 unsigned 형
+d => double,8
+f => float,4
+l,L => long,4
+i,I => int,4
+h,H => short,2
+b,B => byte,1
+
