b20e177ac11a76b0541a6368585231a3f263c83f
gbox3d <gbox3d@gmail.com>
2020-08-26 10:36:43 +0900

    ...

--- day23/ex01/ex01.cpp
+++ day23/ex01/ex01.cpp
@@ -2,19 +2,39 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+typedef struct MyStruct
+{
+    unsigned short x;
+    unsigned short y;
+    unsigned short w;
+    unsigned short h;
+}S_SHEET;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    static unsigned char buf[10000];
+    FILE *fp = fopen("../sheet.bin","rb");
+
+    fseek(fp, 0, SEEK_END);
+    int _size = ftell(fp);
+
+    int _count = _size / sizeof(S_SHEET);
+
+    fseek(fp, 0, SEEK_SET);
+    fread(buf,10000,1,fp);
+    printf("data count : %d\n", _count);
+
+    S_SHEET* ptr = (S_SHEET*)buf;
+    for (int i = 0; i < _count; i++)
+    {
+        printf("%4d,%4d,%4d,%4d\n", ptr->x, ptr->y, ptr->w, ptr->h);
+        ptr++;
+    }    
+    //printf("%4d,%4d,%4d,%4d\n", ptr->x, ptr->y, ptr->w, ptr->h);
+    fclose(fp);
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
